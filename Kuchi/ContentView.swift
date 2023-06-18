bf08900ac52424f50c79288f5e1d284059790c6c
Marvin <marvkipi@gmail.com>
2023-06-18 16:46:36 -0500

    Initial Commit

---
+++ Kuchi/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Kuchi
+//
+//  Created by Marvin on 6/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kuchi/KuchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KuchiApp.swift
+//  Kuchi
+//
+//  Created by Marvin on 6/18/23.
+//
+
+import SwiftUI
+
+@main
+struct KuchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
